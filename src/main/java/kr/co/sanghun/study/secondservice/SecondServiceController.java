ce1e8f51e068ff6352a4af2a4ea70b07be393115
sanghun <sanghun1984@gmail.com>
2022-03-12 14:44:15 +0900

    add sources

--- src/main/java/kr/co/sanghun/study/secondservice/SecondServiceController.java
+++ src/main/java/kr/co/sanghun/study/secondservice/SecondServiceController.java
@@ -1,15 +1,22 @@
 package kr.co.sanghun.study.secondservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RequestMapping("/second-service")
+@RequiredArgsConstructor
 @RestController
 public class SecondServiceController {
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -20,5 +27,11 @@ public class SecondServiceController {
     public String message(@RequestHeader("second-request") String header) {
         log.info(header);
         return "Hello World in Second Service";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("This message is from second service %s", env.getProperty("local.server.port"));
     }
 }
